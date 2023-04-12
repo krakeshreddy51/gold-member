6ef20f4a6895eecfd5e229bcfcc570ebd3e4f93e
Rakesh Reddy <krakeshreddy51@gmail.com>
2023-04-12 04:07:17 +0000

    Wyn

--- print_full_name.py
+++ print_full_name.py
@@ -11,6 +11,6 @@ def print_full_name(first, last):
     # Write your code here
     print("Hello " + first + " " + last + " !You just delved into python.")
 if __name__ == '__main__':
-    first_name = input()
-    last_name = input()
+    first_name = input("your first name: ")
+    last_name = input("your last name: ")
     print_full_name(first_name, last_name)
