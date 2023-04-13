5275fa32579f30d1c799d29cb1ff85bebf69044c
Rakesh Reddy <krakeshreddy51@gmail.com>
2023-04-13 06:41:16 +0000

    Added code to list Multiples of 4

--- ForLoop.py
+++ ForLoop.py
@@ -1,3 +1,34 @@
 colors=['red', 'blue','green', 'ornage']
 for i in colors:
     print(i)
+
+#Tuples    
+point=(1,2,3)
+for i in point:
+    print(i)
+
+#Lists    
+names=['Aron','Akron','Vayu']
+for i in names:
+    print(i)
+  
+#Dictionaries    
+ages={'Kevin':60, 'Bob':59}
+for i in ages:
+    print(i)
+    
+for i,j in ages.items():
+    print(i,j)
+ 
+#Strings    
+for i in 'my_string':
+    print(i)
+
+#Multiples of 4    
+count=1
+while count<=26:
+    if count%4==0:
+        print(count)
+    count+=1
+    
+        
