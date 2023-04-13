6e171ac553844bf8fc24484145c05926c2ef33d4
Rakesh Reddy <krakeshreddy51@gmail.com>
2023-04-13 05:45:04 +0000

    IfElseTest

---
+++ IfelseTest.py
@@ -0,0 +1,12 @@
+if 'a' < 'b':
+    print("condition is true")
+    
+if True:
+    print("it is true")
+else:
+    print("it is false")
+    
+if False:
+    print("It is True")
+else:
+    print("it is False")
